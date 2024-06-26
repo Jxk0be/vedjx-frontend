8138755c1f90162146c0c3e3413abc4a56d72097
Jxk0be <jakeshoffner27@gmail.com>
2024-06-26 11:16:34 -0400

    iphone fix

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="w-full h-screen text-white relative custom-gradient">
-    <video src={KnoxvilleVid} class="w-full z-[5] h-full object-cover" muted autoplay loop />
+    <video src={KnoxvilleVid} class="w-full z-[5] h-full object-cover" muted autoplay loop playsinline controls webkit-playsinline />
     <div class="absolute w-full z-[12] h-full top-0 left-0 bg-black/70" />
     <div class="absolute top-0 w-full h-full flex flex-col justify-center items-center z-[15]">
         <div class="font-bold md:min-w-[400px] min-w-[300px] py-[10px]">
